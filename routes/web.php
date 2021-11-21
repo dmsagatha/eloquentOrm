c803c1dad1bfbf5211b483afd0a920c31b3bce9a
Doris Morales <dmorales55@gmail.com>
2021-11-21 13:59:56 -0500

    Obtener registros por su PK

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,15 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+/**
+ * Buscar un Post por su ID
+ */
+Route::get('buscar/{id}', function (int $id) {
+    return Post::find($id);
 });
